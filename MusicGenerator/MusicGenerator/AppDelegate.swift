9a1b10c3cd76d591906641f06072aeb00840c0c1
henriqueconte <henriqueconte6@gmail.com>
2020-04-14 11:41:54 -0300

    changed xcode project

--- MusicGenerator/MusicGenerator/AppDelegate.swift
+++ MusicGenerator/MusicGenerator/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  MusicGenerator
 //
-//  Created by Henrique Figueiredo Conte on 05/04/20.
+//  Created by Henrique Figueiredo Conte on 14/04/20.
 //  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
 //
 
@@ -10,6 +10,8 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
